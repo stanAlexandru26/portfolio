fac073c8a6a7b0daaf6fd0308b14755f91eb36a0
stan.alexandru26 <stan.alexandru26@gmail.com>
2023-12-14 01:47:49 +0200

    build:add template dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,23 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import vercel from "@astrojs/vercel/serverless";
+import partytown from "@astrojs/partytown";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  site: 'https://www.stefan-stan.dev',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    },
+    speedInsights: {
+      enabled: true
+    }
+  }),
+  integrations: [partytown(), mdx(), sitemap(), compress()]
+});
